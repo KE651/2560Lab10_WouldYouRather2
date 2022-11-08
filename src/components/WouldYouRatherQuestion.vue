f5ff7091f8f122021ca1cfc86cdb0067e090f74b
KE651 <101422949+KE651@users.noreply.github.com>
2022-11-07 20:02:22 -0600

    updated

--- src/components/WouldYouRatherQuestion.vue
+++ src/components/WouldYouRatherQuestion.vue
@@ -3,11 +3,11 @@
     <h2>Please make your choice!</h2>
     <h3>{{ questionData.question }}</h3>
 
-    <input class="button" type="radio" v-model="choice"
+    <input type="radio" class="button" v-model="choice"
            v-bind:value="questionData.answer1" v-on:change="choiceMade">
     <label>{{ questionData.answer1 }}</label>
 
-    <input class="button" type="radio" v-model="choice"
+    <input type="radio" class="button" v-model="choice"
            v-bind:value="questionData.answer2" v-on:change="choiceMade">
     <label>{{ questionData.answer2 }}</label>
     <br><br>
@@ -21,19 +21,22 @@ export default {
   props: {
     questionData: Object
     //props are for data given to child by parent, component should not modify props
-  },
+  }, // end props
+
   data() {
     return {choice:''}
     //choice will update in devtools when radio buttons are selected
     //data function is for data that is used internally to the component
-  },
+  }, // end data
+
   methods: {
     choiceMade() {
       //emit an event 'answer-changed' with value of selection
+      console.log('choiceMade reached ', this.choice, this.questionData.id)
       this.$emit('answer-changed', this.choice, this.questionData.id)
       // name of event is 'answer-changed', convention is for dashes in events
       // $emit(name-of-event, argument1, argument2, ..)
-    }
+    } // end choiceMade
   } // end methods
 } // end export
 // props are for data provided by a parent component (App.vue in this case)
