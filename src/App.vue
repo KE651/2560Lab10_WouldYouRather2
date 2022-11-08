d98b84940f82d0f5582ec0aee440798c13fdea56
KE651 <101422949+KE651@users.noreply.github.com>
2022-11-07 20:01:50 -0600

    updated

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,33 @@
 <template>
   <div id="app">
     <h1>Would you rather...</h1>
-    <would-you-rather
+
+    <WouldYouRather
         v-for="wyrQuestion in questions"
         v-bind:key="wyrQuestion.id"
         v-bind:question="wyrQuestion.question"
         v-bind:answer1="wyrQuestion.answer1"
         v-bind:answer2="wyrQuestion.answer2"
         v-on:answer-changed="answerChanged">
-      <!-- when answer-changed event is received from child component, call the method answerChanged-->
-    </would-you-rather>
+    </WouldYouRather>
+    <!-- when answer-changed event is received from child component, call the method answerChanged-->
     <!-- adds WouldYouRather component -->
     <!-- v-bind from parent data to child props -->
+    <!-- v-bind: [prop in wyr.vue] = ["name in App.vue"]-->
 
     <ul>
+      <li>this is the list</li>
       <li v-bind:key="choice" v-for="choice in choices">{{ choice }}</li>
     </ul>
   </div>
 </template>
-<!-- v-bind: [prop in wyr.vue] = ["name in App.vue"]-->
 
 <script>
+// console.log("app script reached")
 import WouldYouRather from './components/WouldYouRatherQuestion.vue'
 export default {
   name: 'App',
-  components: {WouldYouRather},
+  components: { WouldYouRather },
   data() {
     return {
       questions: [
@@ -41,12 +44,15 @@ export default {
           answer1: 'PUPPIES!!!',
           answer2: 'KITTENS!!!'}
       ],
-      choices: []
+      choices: [],
+      userSelectionMessage:''
       //https://conversationstartersworld/would-you-rather-questions/would-you-rather-questions-for-kids #21
     }
   }, // end data
+
   methods: {
     answerChanged(choice, id) {
+      console.log("answer Changed method reached")
       this.userSelectionMessage = `Thanks!  You chose ${choice} for question ${id}.`
       this.choices.push(choice) // test this to see what happens, from class, doesn't work
       // use id to update
